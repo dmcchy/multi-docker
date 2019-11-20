8cf2aaac19bdefb43711ec4dd6d949da58e686b9
Demby Abella <dmcchy@gmail.com>
2019-11-20 16:08:08 +0800

    sana all

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
           <div className="App">
               <header className="App-header">
                   <img src={logo} className="App-logo" alt="logo" />
+                  <h3>Sana All My Date</h3>
                   <Link to={"/"}>Home</Link>
                   <Link to={"/otherpage"}>Other Page</Link>
               </header>
